53d13a9fd950499310c4ab659e8c2b9d13df0ea1
Connerpang <32760724+Connerpang@users.noreply.github.com>
2017-10-20 11:09:48 +0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,2 +1,24 @@
 /* webcoder assignment JavaScript */
 
+var input = $('#name-id');
+var msg = $('#alert-id');
+input.change(function(){
+	var txtLength = input.val().length;
+	console.log(txtLength);
+	if( txtLength < 4 ){
+		msg.removeClass('hidden');
+	} else {
+		msg.addClass('hidden');
+	}
+});
+
+
+// $("form-group").click(function()){
+// 	alert("Thank you & see you soon!.");
+// });
+
+function myFunction() {
+    var x = document.getElementById("mySubmit").value;
+    document.getElementById("demo").innerHTML = x;
+}
+
