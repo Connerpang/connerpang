b42fba6645625ad7a7f156072392c1f855393b3b
Connerpang <32760724+Connerpang@users.noreply.github.com>
2017-10-22 11:37:09 +0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,24 +1,56 @@
-/* webcoder assignment JavaScript */
-
-var input = $('#name-id');
-var msg = $('#alert-id');
-input.change(function(){
-	var txtLength = input.val().length;
-	console.log(txtLength);
-	if( txtLength < 4 ){
-		msg.removeClass('hidden');
-	} else {
-		msg.addClass('hidden');
-	}
-});
-
-
-// $("form-group").click(function()){
-// 	alert("Thank you & see you soon!.");
-// });
-
-function myFunction() {
-    var x = document.getElementById("mySubmit").value;
-    document.getElementById("demo").innerHTML = x;
-}
-
+$(function () {
+	
+		// on submitting the form
+		$('form').submit(function (event) {
+			// prevent the default action of reloading the page
+			event.preventDefault();
+	
+			var sendData = {};
+			$(event.target.nodeName + ' :input').each(function(){
+				sendData[this.name] = $(this).val();
+			});
+	
+			var posting = $.ajax({
+				type: 'POST',
+				url: $(event.target.nodeName).prop('action'),
+				data: sendData
+			});
+	
+			posting.done(function (response) {
+				console.log(response);
+				
+				if($('#alert-id').length > 0){
+					$('#alert-id').prop('hidden', false);
+				} else {
+					$('form').append('<p>Thank you</p>')
+				}
+				
+				$('form :input').each(function(){
+					$(this).val('');
+				})
+			});
+			posting.fail(function (response) {
+				console.log(response);
+			});
+		});
+	
+	// RESPONSE ALERT WINDOW-------------------------------------------------------------------------------
+	/* include the following HTML to use:
+	<div class="form-group">
+		<button type="submit" class="btn btn-default my-btn form-control" id="submit-id">submit</button>                   
+		<div class="alert alert-danger alert-dismissible fade in" hidden id="alert-id">
+			<button type="button" class="close" id="close-id"><span>&times;</span></button>
+			Thank you! I will get in touch.
+		</div>
+	</div>
+	*/
+	
+		// on clicking the X button
+		$('#close-id').click(function(){
+			// hide the alert panel by adding the hidden property
+			$('#alert-id').prop('hidden', true);
+			// optionally reload the webpage
+			location.reload();
+		});
+	
+	});
